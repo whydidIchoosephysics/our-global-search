9471a30febc112754de7ee4ca54a12a98011d47e
whydidIchoosephysics <ionelvlad123@gmail.com>
2023-03-22 20:18:11 +0000

    Added search bar and map is visualised

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import SearchBar from "./components/SearchBar";
+import Map from "./components/Map";
 
-function App() {
+const App = () => {
+  const [searchedCountry, setSearchedCountry] = useState("");
+
+  const handleSearch = (country) => {
+    setSearchedCountry(country);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Global Search</h1>
+      <SearchBar onSearch={handleSearch} />
+      <Map country={searchedCountry} />
     </div>
   );
-}
+};
 
 export default App;
